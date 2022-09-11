04bf2f6f57f511b0097eba30ccab2c5319104f4f
Jason Cross <jason.m.cross@gmail.com>
2022-09-11 18:08:04 -0400

    More tweaks

--- Lab3/Lab3/Lab3Main.cs
+++ Lab3/Lab3/Lab3Main.cs
@@ -10,11 +10,14 @@ namespace MyNamespace
         {
             Accountant accountant = new Accountant();
             Blacksmith b = new Blacksmith();
+            Manager m = new Manager();
             Owner o = new Owner();
 
             accountant.Update(b, 65000);
+            accountant.Update(m, 75000);
 
             Console.WriteLine(b.getSalary());
+            Console.WriteLine(m.getSalary());
         }
     }
 }
