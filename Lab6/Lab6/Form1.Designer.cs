dbd12e3654639e3e2f428676e3ddea0d9187ce44
DomCarl <dfc5456@psu.edu>
2022-10-02 15:41:08 -0400

    push

--- Lab6/Lab6/Form1.Designer.cs
+++ Lab6/Lab6/Form1.Designer.cs
@@ -29,31 +29,113 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(12, 30);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.Size = new System.Drawing.Size(729, 370);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(262, 420);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 485);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Button 1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(443, 485);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Button 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(352, 485);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Button 3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(747, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(732, 370);
+            this.panel2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Viewing Window";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1131, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Editing Window";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1491, 553);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Lab6/Lab6/Form1.cs
+++ Lab6/Lab6/Form1.cs
@@ -19,13 +19,78 @@ namespace Lab6
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            /**
+             * 
+            int s = 25;
+            int v = 1;
             Graphics g = this.panel1.CreateGraphics();
             Pen blackPen = new Pen(Color.Black, 3);
+            SolidBrush sb = new SolidBrush(Color.Red);
 
-            PointF p1 = new PointF(100.0F, 100.0F);
-            PointF p2 = new PointF(500.0F, 200.0F);
+            Rectangle r1 = new Rectangle(200, 130, 2 * s, 2 * s);
+            Rectangle r2 = new Rectangle(100, 330, 2 * s, 2 * s);
 
-            e.Graphics.DrawLine(blackPen, p1, p2);
-        } 
+            e.Graphics.DrawEllipse(blackPen, r1.X, r1.Y, r1.Width, r1.Height);
+            e.Graphics.DrawEllipse(blackPen, r2.X, r2.Y, r2.Width, r2.Height);
+
+            if (r1.Y > r2.Y)
+            {
+                v = -1;
+            }
+
+            double d = Cosine(new Point(r2.X - r1.X, r2.Y - r1.Y), new Point(v, 0));
+
+            double x = r1.X + s + v * s * d;
+            double y = r1.Y + s + v * s * Math.Sqrt(1 - d * d);
+
+            double x2 = r2.X + s - v * s * d;
+            double y2 = r2.Y + s - v * s * Math.Sqrt(1 - d * d);
+
+            e.Graphics.DrawLine(blackPen, (int)x, (int)y, (int)x2, (int)y2);
+            e.Graphics.FillEllipse(sb, (int)(x - 5), (int)(y - 5), 10, 10);
+
+            Point p = Compute(new Point(r2.X - r1.X, r2.Y - r1.Y), Math.PI / 6);
+            e.Graphics.DrawLine(blackPen, (int)x2, (int)y2, (int)x2 + p.X, (int)y2 + p.Y);
+            p = Compute(new Point(r2.X - r1.X, r2.Y - r1.Y), - Math.PI / 6);
+            e.Graphics.DrawLine(blackPen, (int)x2, (int)y2, (int)x2 + p.X, (int)y2 + p.Y);
+            *
+            */
+        }
+        
+        public double Cosine(Point p1, Point p2)
+        {
+            double d0 = p1.X * p2.X + p1.Y * p2.Y;
+            double d1 = Math.Sqrt(p1.X * p1.X + p1.Y * p1.Y);
+
+            return d0 / d1;
+        }
+
+        public Point Compute(Point p1, double angle)
+        {
+            double d1 = Math.Sqrt(p1.X * p1.X + p1.Y * p1.Y);
+
+            double x = -20 * p1.X / d1;
+            double y = -20 * p1.Y / d1;
+
+            double nx = x * Math.Cos(angle) - y * Math.Sin(angle);
+            double ny = x * Math.Sin(angle) + y * Math.Cos(angle);
+
+            return new Point((int)nx, (int)ny);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
