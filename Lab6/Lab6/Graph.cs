0a7f53e92482e10644ab829237c5a433de725d7a
Jason Cross <jason.m.cross@gmail.com>
2022-10-01 21:54:47 -0400

    Starter files

---
+++ Lab6/Lab6/Graph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Graph : GraphIF
+    {
+        private int id;
+        private List<Vertex> vertices;
+        private List<Edge> edges;
+
+        public Graph(int id)
+        {
+            this.id = id;
+            vertices = new List<Vertex>();
+            edges = new List<Edge>();
+        }
+
+        public void AddVertex(Vertex v)
+        {
+            vertices.Add(v);
+        }
+
+        public void AddEdge(Edge e)
+        {
+            edges.Add(e);
+        }        
+
+        public List<Vertex> GetVertices()
+        {
+            return vertices;
+        }
+
+        public List<Edge> GetEdges()
+        {
+            return edges;
+        }
+
+        public void print(int graphID)
+        {
+            
+        }
+
+        public int getID()
+        {
+            return id;
+        }
+
+        public void setID(int id)
+        {
+            
+        }
+
+        public object Clone()
+        {
+            try
+            {
+                return base.MemberwiseClone();
+            }
+        }
+    }
+}
