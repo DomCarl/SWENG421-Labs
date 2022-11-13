d4b7dad259961f070902921970b42431716fcd60
Jason Cross <jason.m.cross@gmail.com>
2022-11-12 22:24:07 -0500

    It is a start

---
+++ Final-Project/Final-Project/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Final_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.newItemMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.lookupMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMI,
+            this.lookupMI,
+            this.categoriesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMI
+            // 
+            this.fileMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newItemMI,
+            this.closeMI});
+            this.fileMI.Name = "fileMI";
+            this.fileMI.Size = new System.Drawing.Size(37, 20);
+            this.fileMI.Text = "File";
+            // 
+            // newItemMI
+            // 
+            this.newItemMI.Name = "newItemMI";
+            this.newItemMI.Size = new System.Drawing.Size(103, 22);
+            this.newItemMI.Text = "New";
+            // 
+            // closeMI
+            // 
+            this.closeMI.Name = "closeMI";
+            this.closeMI.Size = new System.Drawing.Size(103, 22);
+            this.closeMI.Text = "Close";
+            // 
+            // lookupMI
+            // 
+            this.lookupMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.searchMI});
+            this.lookupMI.Name = "lookupMI";
+            this.lookupMI.Size = new System.Drawing.Size(59, 20);
+            this.lookupMI.Text = "Lookup";
+            // 
+            // searchMI
+            // 
+            this.searchMI.Name = "searchMI";
+            this.searchMI.Size = new System.Drawing.Size(109, 22);
+            this.searchMI.Text = "Search";
+            // 
+            // categoriesToolStripMenuItem
+            // 
+            this.categoriesToolStripMenuItem.Name = "categoriesToolStripMenuItem";
+            this.categoriesToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.categoriesToolStripMenuItem.Text = "Categories";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(675, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Digital Cookbook";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMI;
+        private System.Windows.Forms.ToolStripMenuItem newItemMI;
+        private System.Windows.Forms.ToolStripMenuItem closeMI;
+        private System.Windows.Forms.ToolStripMenuItem lookupMI;
+        private System.Windows.Forms.ToolStripMenuItem searchMI;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem categoriesToolStripMenuItem;
+    }
+}
+
