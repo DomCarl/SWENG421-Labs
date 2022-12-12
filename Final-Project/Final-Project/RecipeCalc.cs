a32a97560fe3cd28e0b332cd22c94d9af007ec0a
Jason Cross <jason.m.cross@gmail.com>
2022-12-12 18:06:41 -0500

    Recipe calc

--- Final-Project/Final-Project/RecipeCalc.cs
+++ Final-Project/Final-Project/RecipeCalc.cs
@@ -24,7 +24,20 @@ namespace Final_Project
 
         private void calcBtn_Click(object sender, EventArgs e)
         {
+            List<Recipe> entreeResults = new List<Recipe>();
+            entreeResults = ListManager.entreeList.FindAll(x => x.name.ToLower().Contains(servTB.Text.ToLower()));
+            List<Recipe> sideResults = new List<Recipe>();
+            sideResults = ListManager.sideList.FindAll(x => x.name.ToLower().Contains(servTB.Text.ToLower()));
+            List<Recipe> dessertResults = new List<Recipe>();
+            dessertResults = ListManager.dessertList.FindAll(x => x.name.ToLower().Contains(servTB.Text.ToLower()));
+            List<Recipe> snackResults = new List<Recipe>();
+            snackResults = ListManager.snackList.FindAll(x => x.name.ToLower().Contains(servTB.Text.ToLower()));
 
+            if (recTB.Text == "" || servTB.Text == "")
+            {
+                MessageBox.Show("Please enter a search term and number of servings.");
+            }
+            else if (entreeResults.Count)
         }
     }
 }
